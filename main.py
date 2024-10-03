ba366ef5c0bdf136a59c5472c272cf338bbc4079
Khushi Pandey <138788874+khushie03@users.noreply.github.com>
2024-10-03 17:24:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,9 @@ import torch
 import pandas as pd
 from transformers import PegasusTokenizer, PegasusForConditionalGeneration
 from transformers import AutoModelForTokenClassification, AutoTokenizer
-
-# Sample text and tags
 text_de = "Google Office is in London Google established in 10092 , Google working on artificiAL iNTELLIGENCE"
 tags = ['O', 'B-PER', 'I-PER', 'B-ORG', 'I-ORG', 'B-LOC', 'I-LOC']
 
-# Define device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model = AutoModelForTokenClassification.from_pretrained("Khushiee/xlm-roberta-base-finetuned-panx-ner-1").to(device)
@@ -47,7 +44,5 @@ def summarize_dialogue(custom_dialogue):
     except Exception as e:
         print(f"An error occurred with the custom model: {e}")
         return None
-
-# Tagging the sample text
 df = tag_text(text_de, tags, model, xlmr_tokenizer)
 print(df)
